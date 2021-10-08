0a71dee6d3b69b54b50070355e8e4a39ed330df6
GiverPlay007 <play.giver.007@gmail.com>
2021-10-07 22:23:05 -0300

    Graphics!

--- src/main/java/me/giverplay/zelda/Game.java
+++ src/main/java/me/giverplay/zelda/Game.java
@@ -1,16 +1,54 @@
 package me.giverplay.zelda;
 
-public class Game implements Runnable {
+import javax.swing.JFrame;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Game extends Canvas implements Runnable {
+
+  public static final int SCREEN_WIDTH = 340;
+  public static final int SCREEN_HEIGHT = 220;
+  public static final int SCREEN_SCALE = 3;
 
   private Thread thread;
   private boolean isRunning;
+
+  private final BufferedImage layer;
+
+  private Game() {
+    setPreferredSize(new Dimension(getScaledWidth(), getScaledHeight()));
+
+    layer = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+    JFrame frame = new JFrame("Game 01 - Zelda");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.add(this);
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+
+    createBufferStrategy(3);
+    frame.setVisible(true);
+  }
 
   public void tick() {
 
   }
 
   public void render() {
+    Graphics graphics = layer.getGraphics();
+    graphics.setColor(Color.GRAY);
+    graphics.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    graphics.dispose();
 
+    graphics = getBufferStrategy().getDrawGraphics();
+    graphics.drawImage(layer, 0, 0, getScaledWidth(), getScaledHeight(), null);
+    graphics.dispose();
+
+    getBufferStrategy().show();
   }
 
   public void start() {
@@ -79,5 +117,17 @@ public class Game implements Runnable {
 
   public static void main(String[] args) {
     new Game().start();
+  }
+
+  public static int getScaledWidth() {
+    return SCREEN_WIDTH * SCREEN_SCALE;
+  }
+
+  public static int getScaledHeight() {
+    return SCREEN_HEIGHT * SCREEN_SCALE;
+  }
+
+  public static int getScreenScale() {
+    return SCREEN_SCALE;
   }
 }
